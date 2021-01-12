5845b2ab546ccb95e55e37a73b0cd254b8e674d0
root <root@3555b0eb15a9>
2021-01-12 02:26:34 +0000

    Day 31 of coding?

--- src/_01_random/_3_dice_roll/DiceRoll.java
+++ src/_01_random/_3_dice_roll/DiceRoll.java
@@ -4,6 +4,8 @@
 package _01_random._3_dice_roll;
 
 import java.awt.Dimension;
+import java.util.Random;
+
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -46,10 +48,13 @@ public class DiceRoll {
 		label.setIcon(oneImg);
 
 		rollButton.addActionListener((e) -> {
-
+			int number = 0;
+			Random ran = new Random();
 			// 1. Make randomChoice equal to a random number between 1 and 6
 			int randomChoice = 0;
-
+			for (int i=0; i<5; i++) {
+				number = ran.nextInt(6+1);
+		}       System.out.println(number);
 			// 2. Fix the code below so that it displays the correct image
 			if (randomChoice == 0) {
 				label.setIcon(oneImg);
