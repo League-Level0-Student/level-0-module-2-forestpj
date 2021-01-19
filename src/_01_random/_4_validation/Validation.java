7a2b0149072ee621f8ab1aa24039e08217558212
root <root@49bef44dd1b4>
2021-01-19 02:25:39 +0000

    Day 32 of coding

--- src/_01_random/_4_validation/Validation.java
+++ src/_01_random/_4_validation/Validation.java
@@ -11,13 +11,28 @@ public class Validation {
 	public static void main(String[] args) {
 		
 		Random randomMaker = new Random();
-		
+		for (int i=0; i<10; i++) {
 		int randomNumber = randomMaker.nextInt(5);
 		
 		System.out.println(randomNumber);
 
 		// 1. Use each value of randomNumber to give the user a random compliment.
-
+		if (randomNumber == 0){
+			JOptionPane.showMessageDialog(null, "you are the best person you can be");
+		}
+		if (randomNumber == 1){
+			JOptionPane.showMessageDialog(null, "You are the most perfect you can be");
+		}
+		if (randomNumber == 2){
+			JOptionPane.showMessageDialog(null, "You are the best person I've met");
+		}
+		if (randomNumber == 3){
+			JOptionPane.showMessageDialog(null, "You are my favorite friend");
+		}
+		if (randomNumber == 4){
+			JOptionPane.showMessageDialog(null, "You are nicer than you think");
+		}
+		}
 		// 2. Repeat all the code above 10 times
 		
 		// 3. Find someone to test out your program. They will like it :)
