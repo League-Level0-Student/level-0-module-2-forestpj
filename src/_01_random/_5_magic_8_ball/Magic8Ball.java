7b335bb42a290912d72d45db5198c318902d6d41
root <root@359d87c6d416>
2021-01-26 02:25:57 +0000

    Day 33 of coding

--- src/_01_random/_5_magic_8_ball/Magic8Ball.java
+++ src/_01_random/_5_magic_8_ball/Magic8Ball.java
@@ -1,37 +1,57 @@
 //    Copyright (c) The League of Amazing Programmers 2013-2019
 //    Level 0
 
-
 package _01_random._5_magic_8_ball;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class Magic8Ball {
 
 	// 1. Make a main method that includes all the steps below
+	public static void main(String[] args) {
 
-	// 2. Get the user to enter a question for the 8 ball to answer
-	
+		Random ran = new Random();
 
-	// 3. Make a variable and initialize it to a random number.
-	//     ** You will need to make a random object!
-	//     Limit the random numbers to be between 0 and 3
+		int randomNum;
 
-	
-	// 4. If the random number is 0
+		randomNum = ran.nextInt(4);
 
-	// -- tell the user "Yes"
+		// 2. Get the user to enter a question for the 8 ball to answer
+		String userquestion = JOptionPane.showInputDialog("Enter something you have a question about");
 
-	// 5. If the random number is 1
+		// 3. Make a variable and initialize it to a random number.
+		// ** You will need to make a random object!
+		// Limit the random numbers to be between 0 and 3
 
-	// -- tell the user "No"
+		// 4. If the random number is 0
+		if (randomNum == 0) {
 
-	// 6. If the random number is 2
+			JOptionPane.showMessageDialog(null, "Yes");
+		}
+		// -- tell the user "Yes"
 
-	// -- tell the user "Maybe you should ask Google?"
+		// 5. If the random number is 1
+		if (randomNum == 1) {
 
-	// 7. If the random number is 3
+			JOptionPane.showMessageDialog(null, "No");
+		}
+		// -- tell the user "No"
 
-	// -- write your own answer
+		// 6. If the random number is 2
+		if (randomNum == 2) {
 
+			JOptionPane.showMessageDialog(null, "Maybe you should ask Google?");
+		}
+		// -- tell the user "Maybe you should ask Google?"
+
+		// 7. If the random number is 3
+		if (randomNum == 3) {
+
+			JOptionPane.showMessageDialog(null, "... Interesting...");
+		}
+		// -- write your own answer
+
+	}
 }
